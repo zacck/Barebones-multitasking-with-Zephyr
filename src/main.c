d71423f47837ad18a8c8a1a3ac4a14e95324ea45
Zacck Osiemo <coderv63@gmail.com>
2024-05-30 06:44:41 +0200

    Add button to application and control different LED

--- src/main.c
+++ src/main.c
@@ -4,11 +4,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include "zephyr/devicetree.h"
 #include <stdio.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/sys/printk.h>
+#include <inttypes.h>
 LOG_MODULE_REGISTER(app, CONFIG_LOG_MAX_LEVEL);
 
 /*Max Entries*/
@@ -64,6 +68,20 @@ K_TIMER_DEFINE(timer1, sensor_expiry_fn, NULL);
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 #define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
+
+
+//Get Button
+#define SW0_NODE  DT_ALIAS(sw0)
+#if  !DT_NODE_HAS_STATUS(SW0_NODE, okay)
+#error "Unsupported board: sw0 devicetree alias not defined"
+#endif
+
+static struct gpio_callback button_cb_data;
+
+
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
+
 
 /*
  * A build error on this line means your board is unsupported.
@@ -71,6 +89,12 @@ K_TIMER_DEFINE(timer1, sensor_expiry_fn, NULL);
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins){
+  printk("Button pressed at %"PRIu32 "\n", k_cycle_get_32());
+}
+
 
 int main(void) {
   /*GPIO STUFF*/
@@ -78,16 +102,48 @@ int main(void) {
   int ret1;
 
   if (!gpio_is_ready_dt(&led))  {
-    LOG_ERR("GPIO1 could not init");
+    LOG_ERR("LED1 could not init");
   }
 
   if (!gpio_is_ready_dt(&led2)) {
-    LOG_ERR("GPIO1 could not init");
+    LOG_ERR("LED2 could not init");
   }
 
-  //Configure both LEDS to start at off
+  if (!gpio_is_ready_dt(&led3)) {
+    LOG_ERR("LED3 could not init");
+  }
+
+  //Configure LEDS to start at off
   ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-  ret1 = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+  ret = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+  ret = gpio_pin_configure_dt(&led3, GPIO_OUTPUT_ACTIVE);
+
+  if(!gpio_is_ready_dt(&button)){
+    printk("Error: button device %s is not ready \n", button.port->name);
+    return  0;
+  }
+
+  ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+  if(ret != 0){
+    printk("Error %d: failed to configure %s pin %d\n", ret, button.port->name, button.pin);
+    return 0;
+  }
+
+  ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+
+  if (ret != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n",
+			ret, button.port->name, button.pin);
+		return 0;
+	}
+
+  gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+  gpio_add_callback(button.port, &button_cb_data);
+  printk("Set up button at %s pin %d\n", button.port->name, button.pin);
+
+
+
+  
  
   /*END GPIO SETUP*/
 
@@ -98,6 +154,11 @@ int main(void) {
   k_timer_start(&timer1, K_MSEC(234), K_MSEC(165));
 
   while (1) {
+    int val = gpio_pin_get_dt(&button);
+
+			if (val >= 0) {
+				gpio_pin_set_dt(&led3, val);
+			}
     // poll the event queue indefinitely
     k_msgq_get(&app_msgq, &evt, K_FOREVER);
 
